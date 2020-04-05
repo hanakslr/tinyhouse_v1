2b9ad3f1c4cb2461f343b0c3663a531f1330360f
hanakslr <58595147+hanakslr@users.noreply.github.com>
2020-04-05 19:01:14 -0400

    Type define server.fetch()
    server.ts - type assert the return value of server.fetch() to be the type that was passed in
    
    types.ts - define the shape of the data we are expecting from the listings query so we can type that too

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {server} from '../../lib/api'
+import {ListingsData} from './types'
 
 const LISTINGS = `
 query Listings {
@@ -21,8 +22,8 @@ interface Props {
 }
 export const Listings = ({title}: Props) => {
     const fetchListings = async () => {
-        const listings = await server.fetch({query: LISTINGS});
-        console.log(listings);
+        const {data} = await server.fetch<ListingsData>({query: LISTINGS});
+        console.log(data);
     };
 
     return (
