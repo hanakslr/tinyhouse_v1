c3f667c60abcb06231f335ac346ca4c32a6c9ce4
hanakslr <58595147+hanakslr@users.noreply.github.com>
2020-04-05 19:22:19 -0400

    Add deleteListing functionality
    types.ts - define the shape of deletelisting data and variables
    
    listings.tsx - add in deleteListing() with a hardcoded id. Use generics to pass in the types of the data and variables
    
    server.ts - update the server.fetch() to have an optional variables field

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {server} from '../../lib/api'
-import {ListingsData} from './types'
+import {ListingsData, DeleteListingData, DeleteListingVariables} from './types'
 
 const LISTINGS = `
 query Listings {
@@ -17,6 +17,14 @@ query Listings {
     }
 }`;
 
+const DELETE_LISTING = `
+mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+        id
+    }
+}
+`;
+
 interface Props {
     title: string
 }
@@ -26,10 +34,21 @@ export const Listings = ({title}: Props) => {
         console.log(data);
     };
 
+    const deleteListing = async () => {
+        const {data} = await server.fetch<DeleteListingData, DeleteListingVariables>({
+            query: DELETE_LISTING,
+            variables: {
+                id: "5e8867b65e36b83cc86ea32a"
+            }
+        });
+        console.log(data);
+    }
+
     return (
         <div>
             <h2>{title}</h2>
             <button onClick={fetchListings}>Query Listings</button>
+            <button onClick={deleteListing}>Delete Listing</button>
         </div>
     ) ;
 };
