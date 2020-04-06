4df07d9e84fbf8a4bfca5c3910c326b8f636ae2d
hanakslr <58595147+hanakslr@users.noreply.github.com>
2020-04-06 07:25:18 -0400

    Fetch the listings on mount of Listings

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {server} from '../../lib/api'
 import {
     ListingsData, 
@@ -40,6 +40,16 @@ export const Listings = ({title}: Props) => {
     // function to update the state. It takes in the initial value
     const [listings, setListings] = useState<Listing[] | null>(null);
 
+    // use effect takes two arguments, the first (required) is the function
+    // to be executed, the second (optional) is the conditions for when
+    // to execute the function. [] indicates on mount, and listed variables
+    // indicates executing when those variables are updated. we can also
+    // return a clean up function that will be called right before the next 
+    // time this effect is exucted
+    useEffect(() => {
+       fetchListings();
+    }, []);
+
     const fetchListings = async () => {
         const {data} = await server.fetch<ListingsData>({query: LISTINGS});
         setListings(data.listings);
@@ -75,7 +85,6 @@ export const Listings = ({title}: Props) => {
         <div>
             <h2>{title}</h2>
             {listingsList}
-            <button onClick={fetchListings}>Query Listings</button>
         </div>
     ) ;
 };
