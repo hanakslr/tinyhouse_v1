36be492d7a29aa3a0881bae75955b60cffd00777
hanakslr <58595147+hanakslr@users.noreply.github.com>
2020-04-28 09:37:10 -0400

    Modifying the user. listing, and booking graphyql types.

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -1,24 +1,79 @@
 import { gql } from "apollo-server-express"
 
+// Re: User:
+// Income and bookings are considered sensitive information so we do not 
+// require them to be provided. If the User that is being returned is the 
+// user that is logged in, we will return those values, if not we will return null values
+
 export const typeDefs = gql`
-         type Viewer {
-           id: ID
-           token: String
-           avatar: String
-           hasWallet: Boolean
-           didRequest: Boolean!
-         }
-         input LogInInput {
-           code: String!
-         }
+  type Booking {
+    id: ID!
+    listing: Listing!
+    tenant: User!
+    checkIn: String!
+    checkOut: String!
+  }
 
-         type Query {
-           authUrl: String!
-           user: String!
-         }
+  type Bookings {
+    total: Int!
+    result: [Booking!]!
+  }
 
-         type Mutation {
-           logIn(input: LogInInput): Viewer!
-           logOut: Viewer!
-         }
-       `;
+  enum ListingType {
+    APARTMENT
+    HOUSE
+  }
+
+  type Listing {
+    id: ID!
+    title: String!
+    description: String!
+    image: String!
+    host: User!
+    type: ListingType!
+    address: String!
+    city: String!
+    bookings(limit: Int!, page: Int!): Bookings
+    bookingsIndex: String!
+    price: Int!
+    numOfGuests: Int!
+  }
+
+  type Listings {
+    total: Int!
+    result: [Listing!]!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    avatar: String!
+    contact: String!
+    hasWallet: Boolean!
+    income: Int             
+    bookings(limit: Int!, page: Int!): Bookings
+    listings(limit: Int!, page: Int!): Listings!
+  }
+
+  type Viewer {
+    id: ID
+    token: String
+    avatar: String
+    hasWallet: Boolean
+    didRequest: Boolean!
+  }
+
+  input LogInInput {
+    code: String!
+  }
+
+  type Query {
+    authUrl: String!
+    user(id: ID!): User!
+  }
+
+  type Mutation {
+    logIn(input: LogInInput): Viewer!
+    logOut: Viewer!
+  }
+`;
