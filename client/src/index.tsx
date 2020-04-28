576d121080448faf141b3102dd9aca3e65e2f02d
hanakslr <58595147+hanakslr@users.noreply.github.com>
2020-04-28 08:11:06 -0400

    Add viewer cookie on client side. Add app header skeleton and spin for when attempting log in.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,5 @@
-import React, {useState} from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useMutation } from "@apollo/react-hooks";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ApolloClient from "apollo-boost";
@@ -13,10 +14,16 @@ import {
   NotFound,
   User,
 } from "./sections";
-import {Viewer} from "./lib/types";
+import { AppHeaderSkeleton, ErrorBanner } from "./lib/components";
+import { LOG_IN } from "./lib/graphql/mutations";
+import {
+  LogIn as LogInData,
+  LogInVariables,
+} from "./lib/graphql/mutations/LogIn/__generated__/LogIn";
+import { Viewer } from "./lib/types";
 import * as serviceWorker from "./serviceWorker";
 import "./styles/index.css";
-import { Affix, Layout } from "antd";
+import { Affix, Layout, Spin } from "antd";
 
 const client = new ApolloClient({
   uri: "/api",
@@ -27,27 +34,60 @@ const initialViewer: Viewer = {
   token: null,
   avatar: null,
   hasWallet: null,
-  didRequest: false
-}
+  didRequest: false,
+};
 
 const App = () => {
   const [viewer, setViewer] = useState<Viewer>(initialViewer);
+  const [logIn, { error }] = useMutation<LogInData, LogInVariables>(LOG_IN, {
+    onCompleted: (data) => {
+      if (data && data.logIn) {
+        setViewer(data.logIn);
+      }
+    },
+  });
+
+  const logInRef = useRef(logIn);
+
+  useEffect(() => {
+    logInRef.current();
+  }, []);
+
+  if (!viewer.didRequest && !error) {
+    return (
+      <Layout className="app-skeleton">
+        <AppHeaderSkeleton />
+        <div className="app-skeleton__spin-section">
+          <Spin size="large" tip="Launching Tinyhouse" />
+        </div>
+      </Layout>
+    );
+  }
+
+  const logInErrorBanner = error ? (
+    <ErrorBanner description="We weren't able to verify if you were logged in. Please try again later." />
+  ) : null;
 
   return (
     <Router>
       <Layout id="app">
+        {logInErrorBanner}
         <Affix offsetTop={0} className="app__affix-header">
           <div>
-            <AppHeader viewer={viewer} setViewer={setViewer}/>
-          </div>          
-        </Affix>        
+            <AppHeader viewer={viewer} setViewer={setViewer} />
+          </div>
+        </Affix>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/host" component={Host} />
           <Route exact path="/listing/:id" component={Listing} />
           <Route exact path="/listings/:location?" component={Listings} />
           <Route exact path="/user/:id" component={User} />
-          <Route exact path="/login" render={props => <Login {...props} setViewer={setViewer} />} />
+          <Route
+            exact
+            path="/login"
+            render={(props) => <Login {...props} setViewer={setViewer} />}
+          />
           <Route component={NotFound} />
         </Switch>
       </Layout>
