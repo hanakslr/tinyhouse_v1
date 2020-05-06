9575f2c21ffc44b9c505ecc02af642d380959080
hanakslr <58595147+hanakslr@users.noreply.github.com>
2020-05-06 09:10:30 -0400

    Build ListingCreateBookings component.

--- client/src/sections/Listing/index.tsx
+++ client/src/sections/Listing/index.tsx
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 import { useParams } from "react-router";
 import { useQuery } from "@apollo/react-hooks";
 import { Col, Layout, Row } from "antd";
+import { Moment } from "moment";
 import { LISTING } from "../../lib/graphql/queries";
 import {
   Listing as ListingData,
   ListingVariables,
 } from "../../lib/graphql/queries/Listing/__generated__/Listing";
 import { PageSkeleton, ErrorBanner } from "../../lib/components";
-import { ListingBookings, ListingDetails } from "./components";
+import {
+  ListingBookings,
+  ListingCreateBooking,
+  ListingDetails,
+} from "./components";
 
 interface MatchParams {
   id: string;
@@ -20,6 +25,9 @@ const PAGE_LIMIT = 3;
 
 export const Listing = () => {
   const [bookingsPage, setBookingsPage] = useState(1);
+  const [checkInDate, setCheckInDate] = useState<Moment | null>(null);
+  const [checkOutDate, setCheckOutDate] = useState<Moment | null>(null);
+
   const { id } = useParams<MatchParams>();
 
   const { data, loading, error } = useQuery<ListingData, ListingVariables>(
@@ -56,7 +64,7 @@ export const Listing = () => {
   const listingDetailsElement = listing ? (
     <ListingDetails listing={listing} />
   ) : null;
-  
+
   const listingBookingsElement = listingBookings ? (
     <ListingBookings
       listingBookings={listingBookings}
@@ -66,12 +74,25 @@ export const Listing = () => {
     />
   ) : null;
 
+  const listingCreateBookingElement = listing ? (
+    <ListingCreateBooking
+      price={listing.price}
+      checkInDate={checkInDate}
+      checkOutDate={checkOutDate}
+      setCheckInDate={setCheckInDate}
+      setCheckOutDate={setCheckOutDate}
+    />
+  ) : null;
+
   return (
     <Content className="listings">
       <Row gutter={24} justify="space-between">
         <Col xs={24} lg={14}>
           {listingDetailsElement}
           {listingBookingsElement}
+        </Col>
+        <Col xs={24} lg={10}>
+          {listingCreateBookingElement}
         </Col>
       </Row>
     </Content>
